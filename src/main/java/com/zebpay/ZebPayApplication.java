8555e22b5a39d74902cbf4e04f72fb3c7ad1622b
sammetas <raamaalayam@gmail.com>
2021-02-24 22:52:57 +0700

    integrated with Eureka

--- src/main/java/com/zebpay/ZebPayApplication.java
+++ src/main/java/com/zebpay/ZebPayApplication.java
@@ -2,10 +2,13 @@ package com.zebpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZebPayApplication {
 
 	public static void main(String[] args) {
@@ -13,6 +16,7 @@ public class ZebPayApplication {
 	}
 
 	@Bean
+	//@LoadBalanced
 	public RestTemplate getRestTemplate(){
 		return  new RestTemplate();
 	}
